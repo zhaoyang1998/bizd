7902f3e07824b3a27a043ba5d05d4ea7b6601861
jin.luo <jin.luo@baishan.com>
2023-03-03 12:12:32 +0800

    V1.0.0 修改排序

--- metion/dao/pointPositionDao.go
+++ metion/dao/pointPositionDao.go
@@ -24,7 +24,7 @@ func GetPointPositionDao(position model.PointPosition) (model.ResponsePagination
 	result := global.DB.Joins("left join t_user on t_user.user_id = t_point_position.implementer_id").
 		Joins("left join t_user t1 on t1.user_id = t_point_position.user_id").
 		Joins("left join t_client client on client.client_id = t_point_position.client_id").
-		Select("t_point_position.*, t_user.user_name as implementer_name, t1.user_name as user_name, client.client_abbreviation as client_abbreviation").Offset((position.PageNumber - 1) * position.PageSize).Limit(position.PageSize).Where(position).Order("updated_at").Find(&pointPositions)
+		Select("t_point_position.*, t_user.user_name as implementer_name, t1.user_name as user_name, client.client_abbreviation as client_abbreviation").Offset((position.PageNumber - 1) * position.PageSize).Limit(position.PageSize).Where(position).Order("updated_at desc").Find(&pointPositions)
 	if result.Error != nil {
 		log.Print(result.Error)
 		return pagination, nil, result.Error
